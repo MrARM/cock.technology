5462803b95aa4b9d28b4b2c8a9b8af7b233f1c62
MrARM <jordan@thesmokinggnu.us>
2020-06-27 19:30:26 -0500

    Add some tweaks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import logo from './chicken.png';
+import gh_logo from './github-corner.svg';
 import './App.css';
 
 function App() {
     const [ classes, setClasses ] = useState('App-logo');
+
+    useEffect(() => {
+        document.title = 'cock.technology';
+    });
 
     const useAudio = url => {
         const [audio] = useState(new Audio(url));
@@ -35,6 +40,9 @@ function App() {
 
     return (
         <div className="App">
+            <div className={'demo version-section'}><a href="https://github.com/MrARM/cock.technology" className="github-corner" aria-label="View source on GitHub">
+                <img className={'octo-arm'} src={gh_logo} /></a>
+            </div>
             <header className="App-header" onClick={() => {
                 setClasses('App-logo App-logo-shake');
                 toggle();
