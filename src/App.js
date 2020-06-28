bd40e83ce7aada6c1198f1f793ded406cfd0c9cf
MrARM <jordan@thesmokinggnu.us>
2020-06-27 19:11:48 -0500

    Change to the website as it stands

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import logo from './chicken.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [ classes, setClasses ] = useState('App-logo');
+
+    const useAudio = url => {
+        const [audio] = useState(new Audio(url));
+        const [playing, setPlaying] = useState(false);
+
+        const toggle = () => setPlaying(!playing);
+
+        useEffect(() => {
+                playing ? audio.play() : audio.pause();
+            },
+            [playing]
+        );
+
+        useEffect(() => {
+            audio.addEventListener('ended', () => {
+                setPlaying(false);
+                setClasses('App-logo');
+            });
+            return () => {
+                audio.removeEventListener('ended', () => setPlaying(false));
+            };
+        }, []);
+
+        return [playing, toggle];
+    };
+
+    const [playing, toggle] = useAudio(require('./vibrate.mp3'));
+
+
+    return (
+        <div className="App">
+            <header className="App-header" onClick={() => {
+                setClasses('App-logo App-logo-shake');
+                toggle();
+            }}>
+                <img src={logo} className={classes} alt="logo" />
+                <p>
+                    Click me
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
